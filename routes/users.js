474eff244311c6c06ccdff570ed7875f05cf4160
Manolis <gkouem313@gmail.com>
2021-03-02 19:36:10 +0200

    basic register functionality

--- routes/users.js
+++ routes/users.js
@@ -1,56 +1,52 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../models/user');
+const User = require("../models/user");
 
 // get all
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const users = await User.find();
     res.send(users);
   } catch (error) {
-    res.status(500).json({message: error})
+    res.status(500).json({ message: error });
   }
-})
+});
 
 // get one
 router.get("/:id", async (req, res) => {
   try {
-    const user = await User.findById(req.params.id)
+    const user = await User.findById(req.params.id);
     if (!user) {
-      return res.status(404).json({message: 'user missing'})
-    } 
-    res.send(user)
-  } catch (error) {
-
-  }
+      return res.status(404).json({ message: "user missing" });
+    }
+    res.send(user);
+  } catch (error) {}
 });
 
 // create one
 router.post("/", async (req, res) => {
+  console.log(req.body);
   const user = new User({
-    username: "manolis",
-    email: "test@test.com",
-    password: 'test1234'
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
   });
   try {
     const newUser = await user.save();
     res.status(201).json(newUser);
   } catch (error) {
-    res.status(500).json({message: error})
+    res.status(500).json({ message: error });
   }
 });
 
 // update one
-router.patch("/:id", (req, res) => {
-
-});
+router.patch("/:id", (req, res) => {});
 
 // delete one
 router.delete("/:id", async (req, res) => {
   const user = await User.findById(req.params.id);
-  res.json({message: 'user deleted'})
+  res.json({ message: "user deleted" });
   user.remove();
 });
-
 
 module.exports = router;
